58001a5c5dee51d7562a1f7995014e260718cad9
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-24 21:20:56 +0300

    Update Scraper

--- src/SAS.ScrapingManagementService.Domain/Scrapers/Entities/Scraper.cs
+++ src/SAS.ScrapingManagementService.Domain/Scrapers/Entities/Scraper.cs
@@ -6,8 +6,16 @@ namespace SAS.ScrapingManagementService.Domain.Scrapers.Entities
     public class Scraper  : BaseEntity<Guid>
     {
         public String ScraperName { get; set; }
-        //public DateTime ScraperName { get; set; }
+        public string Hostname { get; set; } // Useful to identify the host or node
 
+        public string IPAddress { get; set; } // Optional, for monitoring or auditing
+
+        public DateTime RegisteredAt { get; set; } // When it registered with the master
+        //public DateTime LastHeartbeat { get; set; } // Last time the agent sent a heartbeat (or pinged the master)
+
+        public bool IsActive { get; set; } // Whether it's currently connected/available
+
+        public int TasksHandled { get; set; } // Number of tasks this agent handled
 
     }
 

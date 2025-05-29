64bfc852f73f5c2143ec4a26ebcbbaa73eb06211
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-29 15:21:18 +0300

    Add Prsentation Project

---
+++ src/SAS.ScrapingManagementService.Presentation/AssemblyReference/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace SAS.ScrapingManagementService.Presentation.AssemblyReference
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+
+    }
+}

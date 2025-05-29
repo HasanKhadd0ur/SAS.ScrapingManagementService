81dedebd2281ffe98dfc4815a4da63ed0ab6d622
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-29 15:51:05 +0300

    Add Initial DB Context

---
+++ src/SAS.ScrapingManagementService.Infrastructure/SAS.ScrapingManagementService.Infrastructure.Persistence/AppDataContext/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SAS.ScrapingManagementService.SharedKernel.DomainEvents;
+
+namespace SAS.ScrapingManagementService.Infrastructure.Persistence.AppDataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+
+        }
+
+        // DbSets for entities
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            modelBuilder.Ignore<List<IDomainEvent>>();
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.HasAnnotation("Relational:Collation", "Arabic_CI_AS");
+
+        }
+
+    }
+}

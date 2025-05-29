dc3e9580e4ba6494796a65dd34797f3d5ec2768a
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-29 15:20:06 +0300

    Update Base Dto

--- src/SAS.ScrapingManagementService.Application/Common/BaseDTO.cs
+++ src/SAS.ScrapingManagementService.Application/Common/BaseDTO.cs
@@ -6,7 +6,9 @@
     }
     public class PaginatedDTO<T> : BaseDTO<T>
     {
+        public IEnumerable<T> Items { get; init; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
+        //public int TotalCount { get; init; }
     }
 }

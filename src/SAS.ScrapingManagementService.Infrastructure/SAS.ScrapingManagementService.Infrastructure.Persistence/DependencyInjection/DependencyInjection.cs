7cb546920f0e54fdd8e9cb308ba2c0060be077a8
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-29 15:51:24 +0300

    Add Di, Base Repo

---
+++ src/SAS.ScrapingManagementService.Infrastructure/SAS.ScrapingManagementService.Infrastructure.Persistence/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SAS.ScrapingManagementService.Domain.DataSources.Repositories;
+using SAS.ScrapingManagementService.Infrastructure.Persistence.AppDataContext;
+using SAS.ScrapingManagementService.Infrastructure.Persistence.Repositories.Base;
+using SAS.ScrapingManagementService.Infrastructure.Persistence.Repositories.DataSources;
+using SAS.ScrapingManagementService.Infrastructure.Persistence.UoW;
+using SAS.ScrapingManagementService.SharedKernel.Repositories;
+using SAS.ScrapingManagementService.SharedKernel.Utilities;
+
+namespace SAS.ScrapingManagementService.Infrastructure.Persistence.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDataContext(configuration)
+                .AddRepositories()
+                .AddUOW();
+
+            return services;
+        }
+
+
+
+
+        #region Register UOW 
+
+        private static IServiceCollection AddUOW(this IServiceCollection services)
+        {
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+        #endregion Register UOW 
+
+        #region Register Repositories
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+
+            services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
+            services.AddScoped<IDataSourceRepository, DataSourceRepository>();
+            return services;
+
+        }
+
+
+        #endregion Register Repositoryies
+
+        #region Register Data context 
+        private static IServiceCollection AddDataContext(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+
+        }
+
+        #endregion Register Data Context 
+    }
+}
---
+++ src/SAS.ScrapingManagementService.Infrastructure/SAS.ScrapingManagementService.Infrastructure.Persistence/Repositories/Base/SpecificationEvaluator.cs
@@ -0,0 +1,58 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SAS.ScrapingManagementService.SharedKernel.Entities;
+using SAS.ScrapingManagementService.SharedKernel.Utilities;
+
+
+namespace SAS.ScrapingManagementService.Infrastructure.Persistence.Repositories.Base
+{
+    public class SpecificationEvaluator<T, TId> where T : BaseEntity<TId>
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+        {
+            var query = inputQuery;
+            if (specification is not null)
+            {
+                // modify the IQueryable using the specification's criteria expression
+                if (specification.Criteria != null)
+                {
+                    query = query.Where(specification.Criteria);
+                }
+
+                // Includes all expression-based includes
+                query = specification.Includes.Aggregate(
+                    query,
+                    (current, include) => current.Include(include));
+
+                // Include any string-based include statements
+                query = specification.IncludeStrings.Aggregate(
+                    query,
+                    (current, include) => current.Include(include));
+
+                // Apply ordering if expressions are set
+                if (specification.OrderBy != null)
+                {
+                    query = query.OrderBy(specification.OrderBy);
+                }
+                else if (specification.OrderByDescending != null)
+                {
+                    query = query.OrderByDescending(specification.OrderByDescending);
+                }
+
+                //if (specification.GroupBy is not null)
+                //{
+                //    query = query.GroupBy(specification.GroupBy).SelectMany(x => x);
+                //}
+
+                // Apply paging if enabled
+                if (specification.IsPagingEnabled)
+                {
+                    query = query.Skip(specification.Skip)
+                                 .Take(specification.Take);
+                }
+            }
+            return query;
+        }
+
+    }
+}

542772c81a49940c1a59fa86f6153ac0f331509c
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-31 16:59:13 +0300

    Add Scraping Domain Controller

--- src/SAS.ScrapingManagementService.Presentation/Controllers/DataSources/ScrapingDomainsController.cs
+++ src/SAS.ScrapingManagementService.Presentation/Controllers/DataSources/ScrapingDomainsController.cs
@@ -1,34 +1,34 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SAS.ScrapingManagementService.Application.DataSources.UseCases.Commands.AddDataSource;
-using SAS.ScrapingManagementService.Application.DataSources.UseCases.Commands.DeleteDataSource;
-using SAS.ScrapingManagementService.Application.DataSources.UseCases.Commands.UpdateDataSource;
 using SAS.ScrapingManagementService.Application.DataSources.UseCases.Queries;
-using SAS.ScrapingManagementService.Application.DataSources.UseCases.Queries.GetAllDataSources;
+using SAS.ScrapingManagementService.Application.ScrapingDomains.UseCases.Commands.CreateScrapingDomain;
+using SAS.ScrapingManagementService.Application.ScrapingDomains.UseCases.Commands.DeleteScrapingDomain;
+using SAS.ScrapingManagementService.Application.ScrapingDomains.UseCases.Commands.UpdateScrapingDomain;
+using SAS.ScrapingManagementService.Application.ScrapingDomains.UseCases.Queries.GetAllDomain;
 using SAS.ScrapingManagementService.Presentation.Controllers.ApiBase;
 
 namespace SAS.ScrapingManagementService.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class DataSourcesController : APIController
+    public class ScrapingDomainsController : APIController
     {
         private readonly IMediator _mediator;
 
-        public DataSourcesController(IMediator mediator)
+        public ScrapingDomainsController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add([FromBody] AddDataSourceCommand command)
+        public async Task<IActionResult> Create([FromBody] CreateScrapingDomainCommand command)
         {
             var result = await _mediator.Send(command);
             return HandleResult(result);
         }
 
         [HttpPut("{id:guid}")]
-        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateDataSourceCommand command)
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateScrapingDomainCommand command)
         {
             var result = await _mediator.Send(command);
             return HandleResult(result);
@@ -37,23 +37,22 @@ namespace SAS.ScrapingManagementService.API.Controllers
         [HttpDelete("{id:guid}")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            var result = await _mediator.Send(new DeleteDataSourceCommand(id));
+            var result = await _mediator.Send(new DeleteScrapingDomainCommand(id));
             return HandleResult(result);
         }
 
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetById(Guid id)
         {
-            var result = await _mediator.Send(new GetDataSourceByIdQuery(id));
+            var result = await _mediator.Send(new GetDomainByIdQuery(id));
             return HandleResult(result);
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] int? pageNumber , [FromQuery] int? pageSize)
+        public async Task<IActionResult> GetAll([FromQuery] int? pageNumber, [FromQuery] int? pageSize)
         {
-            var result = await _mediator.Send(new GetAllDataSourcesQuery(pageNumber, pageSize));
+            var result = await _mediator.Send(new GetAllDomainsQuery(pageNumber, pageSize));
             return HandleResult(result);
         }
     }
-
 }

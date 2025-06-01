3dd742b2fe079178e60889de8578388801f061aa
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-06-01 13:53:47 +0300

    Add Scrapig Controller

---
+++ src/SAS.ScrapingManagementService.Presentation/Controllers/Scrapers/ScrapersController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SAS.ScrapingManagementService.Application.Scrapers.UseCases.Commands.ConnectScraper;
+using SAS.ScrapingManagementService.Presentation.Controllers.ApiBase;
+
+namespace SAS.ScrapingManagementService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ScrapersController : APIController
+    {
+        private readonly IMediator _mediator;
+
+        public ScrapersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("connect")]
+        public async Task<IActionResult> Connect([FromBody] ConnectScraperCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return HandleResult(result);
+        }
+    }
+}
---
+++ src/SAS.ScrapingManagementService.Presentation/Controllers/Scrapers/ScrapingTasksController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SAS.ScrapingManagementService.Application.Scrapers.UseCases.Commands.AssignScrapingExecutor;
+using SAS.ScrapingManagementService.Application.Scrapers.UseCases.Commands.CompleteScrapingTask;
+using SAS.ScrapingManagementService.Presentation.Controllers.ApiBase;
+
+namespace SAS.ScrapingManagementService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ScrapingTasksController : APIController
+    {
+        private readonly IMediator _mediator;
+
+        public ScrapingTasksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("{id:guid}/complete")]
+        public async Task<IActionResult> CompleteScrapingTask(Guid id)
+        {
+            var result = await _mediator.Send(new CompleteScrapingTaskCommand(id));
+            return HandleResult(result);
+        }
+
+        [HttpPost("assign")]
+        public async Task<IActionResult> AssignExecutor([FromBody] AssignScrapingExecutorCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return HandleResult(result);
+        }
+    }
+}

b87faf7a9d4a906e9430c497fd9b2244c5ce1a3f
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-02 22:54:41 +0300

    Update Data Source Type UseCases

--- src/SAS.ScrapingManagementService.Application/ApplicationMappers/MappingProfile.cs
+++ src/SAS.ScrapingManagementService.Application/ApplicationMappers/MappingProfile.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using SAS.ScrapingManagementService.Application.DataSources.Common;
 using SAS.ScrapingManagementService.Application.DataSources.UseCases.Commands.AddDataSource;
 using SAS.ScrapingManagementService.Application.DataSources.UseCases.Commands.UpdateDataSource;
+using SAS.ScrapingManagementService.Application.DataSourceTypes.Common;
 using SAS.ScrapingManagementService.Application.Platforms.Common;
 using SAS.ScrapingManagementService.Application.ScrapingDomains.Common;
 using SAS.ScrapingManagementService.Application.ScrapingDomains.UseCases.Commands.CreateScrapingDomain;
@@ -28,7 +29,10 @@ namespace SAS.ScrapingManagementService.Application.Common.Mappings
             CreateMap<ScrapingDomain, ScrapingDomainDto>().ReverseMap();
             CreateMap<CreateScrapingDomainCommand, ScrapingDomain>();
             CreateMap<UpdateScrapingDomainCommand, ScrapingDomain>();
-
+            
+            // ============ DataSourceTypes ===========
+            CreateMap<DataSourceType, DataSourceTypeDto>().ReverseMap();
+            
         }
     }
 }
---
+++ src/SAS.ScrapingManagementService.Application/DataSourceTypes/UseCases/Commands/AddDataSourceType/AddDataSourceTypeCommand.cs
@@ -0,0 +1,7 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace SAS.ScrapingManagementService.Application.DataSourceTypes.UseCases.Commands.AddDataSourceType
+{
+    public record AddDataSourceTypeCommand(string Name) : IRequest<Result<Guid>>;
+}

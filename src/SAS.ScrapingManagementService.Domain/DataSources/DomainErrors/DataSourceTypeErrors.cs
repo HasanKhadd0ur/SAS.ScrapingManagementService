5ac283313d237f4acf7b6812b3d260e3bfc0bfff
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-02 22:56:16 +0300

    Update Domain Errors

--- src/SAS.ScrapingManagementService.Domain/DataSources/DomainErrors/DataSourceTypeErrors.cs
+++ src/SAS.ScrapingManagementService.Domain/DataSources/DomainErrors/DataSourceTypeErrors.cs
@@ -6,4 +6,6 @@ public static class DataSourceTypeErrors
 {
     public static readonly DomainError UnExistType =
          new("DataSourceTypeError.UnExistType", "Data Source Type un exist.");
+    public static DomainError AlreadyExists(string name) =>
+            new("DataSourceTypeError.AlreadyExists", $"A Data Source Type  with the name '{name}' already exists.");
 }

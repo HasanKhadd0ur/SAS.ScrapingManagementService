8b64c2b055322dcf6676483d86857e1be39d44b3
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-14 09:53:05 +0300

    Add Settings for Peipline

---
+++ src/SAS.ScrapingManagementService.Domain/Settings/Entities/PipelineStage.cs
@@ -0,0 +1,16 @@
+﻿using SAS.SharedKernel.Entities;
+
+namespace SAS.ScrapingManagementService.Domain.Settings.Entities
+{
+    public class PipelineStage : BaseEntity<Guid>
+    {
+        public string StageName { get; set; }  // e.g. "NormalizeTextStage"
+
+        public int Order { get; set; }  // Execution order of the stage
+
+        public Guid PipelineConfigId { get; set; }
+        public PipelineConfig PipelineConfig { get; set; }
+
+        public string ParametersJson { get; set; }
+    }
+}
